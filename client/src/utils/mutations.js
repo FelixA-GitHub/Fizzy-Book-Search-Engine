01d44a688eb654fa516616b2a8a06fc754691771
Your Name <felix.acevedo.tech@gmail.com>
2022-06-24 12:18:33 -0400

    updated signupform and login form

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,58 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook([$authors]: String!, $bookId: ID!) {
+    addBook(authors: [$authors], bookId: $bookId) {
+      [authors]
+      description
+      savedBook{
+        title
+        bookId
+        image
+        link
+      }
+      
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+          [author]
+          description
+          title
+          bookId
+          image
+          link
+      }
+    }
+  }
+`;
