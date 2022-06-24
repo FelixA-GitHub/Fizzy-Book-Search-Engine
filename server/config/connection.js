89c2a688ec585fd2c0700f05ae3325000142094c
Your Name <felix.acevedo.tech@gmail.com>
2022-06-24 16:37:39 -0400

    pushing to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/Book-Search-Engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Book-Search-Engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
