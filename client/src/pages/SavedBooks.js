3c9838471c6c3f9942434b12e51da68373352d78
Your Name <felix.acevedo.tech@gmail.com>
2022-06-24 13:51:16 -0400

    working on using GET_ME query

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import { deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-
-  
+  const userData = useQuery()
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
