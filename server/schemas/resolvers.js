b697461b544a6d0690e153e30e1939a481995bc3
Your Name <felix.acevedo.tech@gmail.com>
2022-06-24 15:04:04 -0400

    working on saveBook in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,58 @@
-const { gql } = require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express');
 // import user model
 const { User } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
 
-const resolvers = gql`
+const resolvers = {
     Query: {
-        getSingleUser(parent, args, context, info) {
-            return getSingleUser;
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password'));
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
         }
     },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+          },
+          saveBook: async (parent, { user, body }, context) => {
+            if (context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+              );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Incorrect credentials');
+          }
+    }
+}
 
-`
 
 module.exports = resolvers;
